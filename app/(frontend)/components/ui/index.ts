087295f8959598d6b7c5654c775ca5027c2b401e
shimdokite <shimdokite@gmail.com>
2026-05-14 15:19:57 +0900

    Feature: test 도메인 추가

--- app/(frontend)/components/ui/index.ts
+++ app/(frontend)/components/ui/index.ts
@@ -1,3 +1,3 @@
-export { default as Input, SearchIcon } from "./input/input";
-export { default as Popover } from "./popover/popover";
-export { default as SegmentedControl } from "./segmented-control/segmented-control";
+export { default as Input, SearchIcon } from "./input";
+export { default as Popover } from "./popover";
+export { default as SegmentedControl } from "./segmented-control";
--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,5 +1,3 @@
-import Example from "./components/Example";
-
 export default function Home() {
-  return <Example />;
+  return <></>;
 }
